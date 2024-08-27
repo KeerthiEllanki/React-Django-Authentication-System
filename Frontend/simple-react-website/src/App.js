e183165cb3dd89aaececb72287734b4f9fce8538
Keerthi Reddy Ellanki <58471970+KeerthiEllanki@users.noreply.github.com>
2024-08-27 08:12:22 -0400

    Frontend Code Commit

---
+++ Frontend/simple-react-website/src/App.js
@@ -0,0 +1,26 @@
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import React, { useState } from 'react';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import './Styles/App.css'; 
+
+function App() {
+  const [isSignup, setIsSignup] = useState(true);
+
+  return (
+    <div className="container">
+      <Header />
+      <h1>Welcome</h1>
+      <div className="button-container">
+      <button onClick={() => setIsSignup(true)}>Signup</button>
+      <button onClick={() => setIsSignup(false)}>Login</button>
+      </div>
+      {isSignup ? <Signup /> : <Login />}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/simple-react-website/src/Components/Signup.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8000/api/signup/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        alert(data.message);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      <label>Email:</label>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Signup;
