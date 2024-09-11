ad0bfa7a37857eed332534fd44d315dbdb74c026
Keerthi Reddy Ellanki <58471970+KeerthiEllanki@users.noreply.github.com>
2024-09-11 08:37:09 -0400

    Update Footer.js

--- Frontend/simple-react-website/src/Components/Footer.js
+++ Frontend/simple-react-website/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2024 My Simple Website</p>
+      <p>&copy; 2024 My Simple Website_Keerthi Reddy Ellanki</p>
     </footer>
   );
 }
